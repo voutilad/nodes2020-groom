2212e002f88a3115a8026a6335bcc7c2781e695a
Dave Voutila <voutilad@gmail.com>
2020-10-06 10:40:19 -0400

    fix path to events cypher

--- create-4.0.sh
+++ create-4.0.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+EVENTS_CYPHER="./cypher/events.cypher"
 BOOTSTRAP_SERVER_URL="pkc-lo7py.northamerica-northeast1.gcp.confluent.cloud:9092"
 API_KEY="AKMM3HDV7U35BYEO"
 API_SECRET="$(gcloud secrets versions access latest --secret=groom-${API_KEY})"
 JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${API_KEY}\" password=\"${API_SECRET}\";"
-CYPHER="$(cat events.cypher | grep -v '//' | tr '\r\n\t' ' ')"
+CYPHER="$(cat ${EVENTS_CYPHER} | grep -v '//' | tr '\r\n\t' ' ')"
 
 printf "Using Cypher:\n${CYPHER}\n"
 
