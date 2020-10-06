a5ba4ac0bbc362c3d72d414f7d97b53b6c888b37
Dave Voutila <voutilad@gmail.com>
2020-10-06 14:50:37 -0400

    Confluent key changed, needs update

--- create-4.0.sh
+++ create-4.0.sh
@@ -2,7 +2,7 @@
 
 EVENTS_CYPHER="./cypher/events.cypher"
 BOOTSTRAP_SERVER_URL="pkc-lo7py.northamerica-northeast1.gcp.confluent.cloud:9092"
-API_KEY="AKMM3HDV7U35BYEO"
+API_KEY="UQJMRNNQ3PSAFO7M"
 API_SECRET="$(sh fetch.sh)"
 JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${API_KEY}\" password=\"${API_SECRET}\";"
 CYPHER="$(cat ${EVENTS_CYPHER} | grep -v '//' | tr '\r\n\t' ' ')"
