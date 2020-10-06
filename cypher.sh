a6d7c9b08c6bc504a022866d893249587ea615dc
Dave Voutila <voutilad@gmail.com>
2020-10-06 14:49:29 -0400

    routing issues with connections, paths
    Until I fix the neo4j config, use bolt+s.
    
    Properly make a valid path to the scripts, too.

--- cypher.sh
+++ cypher.sh
@@ -5,7 +5,7 @@ if [[ $# -eq 1 ]]; then
     docker run --rm -it --name cypher-exec \
            -v "$(pwd)/cypher:/cypher" -u root --entrypoint /bin/bash \
            neo4j:4.0-enterprise \
-           -c "cypher-shell -d groom -u neo4j -a neo4j+s://${NEO4J_HOST} -f /cypher/$1"
+           -c "cypher-shell -d groom -u neo4j -a bolt+s://${NEO4J_HOST} -f /$1"
 else
     echo "cypher.sh -- usage: $0 [cypher file]" 1>&2;
     exit 1;
